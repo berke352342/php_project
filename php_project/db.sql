ff55606a50070e1a1877affba616d094bb6015a2
berkereis31 <berkcark4@gmail.com>
2025-11-29 21:34:19 +0300

    Ppevent duplicate admin entriees in db.sql

--- php_project/db.sql
+++ php_project/db.sql
@@ -1,7 +1,10 @@
 -- the database that we will use
 USE Ahmet200575280;
 
- -- product table
+-- to avoid duplicate admin entry during multiple runs
+DELETE FROM admins WHERE email = 'test@musclefuel.com';
+
+-- product table
 CREATE TABLE IF NOT EXISTS products (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
@@ -12,10 +15,7 @@ CREATE TABLE IF NOT EXISTS products (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-
-
 -- admin table
-
 CREATE TABLE IF NOT EXISTS admins (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
@@ -24,9 +24,6 @@ CREATE TABLE IF NOT EXISTS admins (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-INSERT INTO admins (name, email, password) VALUES
-('TestAdmin', 'test@musclefuel.com', '$2y$10$KIXh6e7/CNhhTvGdVQPlOu2DQvE6y50g.9YksjFg6CwVf3QmMSk1a'); -- this is the default password: 1234
 
 --  it will be only 3 product
 INSERT INTO products (name, description, price, image) VALUES
